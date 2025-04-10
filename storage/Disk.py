0e3425db3813475705b7ac850250a072a619ec39
trio <amal.benny@liquidlab.in>
2025-04-10 14:09:03 +0530

    Add files via upload

--- storage/Disk.py
+++ storage/Disk.py
@@ -1,10 +1,22 @@
 import os
 import cv2
 
-def save_frame_to_binary(frame, file_path="spaceview.jpg"):
-    """Save frame as a binary JPEG file."""
-    cv2.imwrite(file_path, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 85])  # 85% quality
+class spaceViewStorage:
+    def save(self, frame, spaceID):
+        """Save frame as a binary JPEG file."""
+        cv2.imwrite(f'spaceViewOf:{spaceID}.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 85])  # 85% quality
 
-def load_frame_from_binary(file_path="spaceview.jpg"):
-    """Load the latest frame from disk."""
-    return cv2.imread(file_path) if os.path.exists(file_path) else None
+    def get(self,spaceID):
+        """Load the latest frame from disk."""
+        return cv2.imread(f'spaceViewOf:{spaceID}.jpg') if os.path.exists(f'spaceViewOf:{spaceID}.jpg') else None
+
+
+class licensePlateStorage:
+    def save(self,frame, spaceID):
+        """Save frame as a binary JPEG file."""
+        cv2.imwrite(f'licensePlateInSpace:{spaceID}.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 85])  # 85% quality
+
+    def get(self,spaceID):
+        """Load the latest frame from disk."""
+        return cv2.imread(f'licensePlateInSpace:{spaceID}.jpg') if os.path.exists(f'licensePlateInSpace:{spaceID}.jpg') else None
+
